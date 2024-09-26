ddefd76e65d3e960176e256eb97a544caaf75c8f
yliang <yliang6@smith.edu>
2024-09-26 08:13:25 +0800

    Add cannedWord

--- Conversation.java
+++ Conversation.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 
 class Conversation {
   
   private int round;
-  
+  private Random ran;
+
   public Conversation(int a){
     this.round = a;
+    this.ran = new Random();
   }
 
   public String Response(){
-
     Scanner inp = new Scanner(System.in);
     String inputString = inp.nextLine();
     String[] words = inputString.split(" ");
     String responseString = "";
-    ArrayList<String> mirrorWords = new ArrayList<>();
+    //randomly choose how to responde.
+    int randomNumber = ran.nextInt(2) + 1;
+    switch(randomNumber){
+      case 1:
+        ArrayList<String> mirrorWords = new ArrayList<>();
         mirrorWords.add("I");
         mirrorWords.add("me");
         mirrorWords.add("am");
@@ -23,15 +29,21 @@ class Conversation {
         mirrorWords.add("my");
         mirrorWords.add("your");
 
-    for(String word : words){
-      if (mirrorWords.contains(word)){
-        responseString += mirrorWord(word)+" ";
-      }
-      else{
-        responseString += word+" ";
-      }
+        for(String word : words){
+          if (mirrorWords.contains(word)){
+            responseString += mirrorWord(word) + " ";
+          } else {
+            responseString += word + " ";
+          }
+        }
+        return responseString;
+      
+      case 2:
+        String response2 = cannedWord();
+        System.out.println(response2);
+        break;
     }
-    return responseString;
+    return "";
   }
 
   public String mirrorWord(String a){
@@ -50,10 +62,19 @@ class Conversation {
         return "my";
       default:
         return a;
-
     }
   }
 
+  public String cannedWord(){
+    String[] cannedResponses = {
+      "Interesting...",
+      "Tell me more.",
+      "I see.",
+      "Fascinating."
+    };
+    int index = ran.nextInt(cannedResponses.length);
+    return cannedResponses[index];
+  }
 
   public static void main(String[] arguments) {
     System.out.println("Enter your round number:");
@@ -61,10 +82,12 @@ class Conversation {
     int a = sc.nextInt();
     System.out.println("Hey, what's on your mind?");
     Conversation conversation = new Conversation(a);
-    for(int j=0; j<=a-1;j++){
-    String printString = conversation.Response();
-    System.out.println(printString);
+    
+    for (int j = 0; j < a; j++) {
+      String printString = conversation.Response();
+      System.out.println(printString);
     }
-    System.out.println("See yah!");
+    
+    System.out.println("See Yah!");
   }
 }
